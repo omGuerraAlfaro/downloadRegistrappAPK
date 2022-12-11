c52bdf750ecfca197ef42fb8e6431caaf0003141
Omar Guerra Alfaro <om.guerra@duocuc.cl>
2022-12-11 16:42:12 -0300

    aaaaaaa

--- js/userAgent.js
+++ js/userAgent.js
@@ -2,7 +2,7 @@
 window.addEventListener('load', () => {
     const tipo = document.getElementById(`tipoUserAgent`);
     if (navigator.userAgent.includes('Windows')) { // (xxx && 'Windows') se muestra... verificar palabra cable para telefonos
-        tipo.innerHTML = `<img src="https://bitacoradelasavesdechile.files.wordpress.com/2011/10/perrito2.jpg" alt="">`
+        tipo.innerHTML = `<img src="https://i.postimg.cc/QdjJ9jp3/11111.jpg" alt="">`
     } else {
         tipo.innerHTML =    `<a href="http://www.google.com">
                                 <div class="contenedorAPK">
