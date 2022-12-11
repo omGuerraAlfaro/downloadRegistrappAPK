eee58e798139777fd6df7c739eca0b7e9a4c32e0
Omar Guerra Alfaro <om.guerra@duocuc.cl>
2022-12-11 16:03:57 -0300

    .....

--- js/userAgent.js
+++ js/userAgent.js
@@ -4,11 +4,13 @@ window.addEventListener('load', () => {
     if (navigator.userAgent.includes('Mobile')) {
         tipo.innerHTML =    `<a href="http://www.google.com">
                                 <div class="contenedorAPK">
-                                   <button class="buttonAnimated"><span>Descargar Aquí</span></button>
+                                   <button class="buttonAnimated">
+                                        <span>Descargar Aquí</span>
+                                   </button>
                                 </div>
                             </a>`
     } else {
-        tipo.innerHTML = `<img src="https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.redobservadores.cl%2F%3Fdslc_downloads%3Dperrito&psig=AOvVaw2o80sC6XiK1fCDL4S4yTKO&ust=1670871571247000&source=images&cd=vfe&ved=0CA8QjRxqFwoTCICaseWf8vsCFQAAAAAdAAAAABAE" alt="">`
+        tipo.innerHTML = `<img src="https://bitacoradelasavesdechile.files.wordpress.com/2011/10/perrito2.jpg" alt="">`
     }
 });
 
