9a3eab01e6658df95d2daac33c721b6ce4c04fda
Omar Guerra Alfaro <om.guerra@duocuc.cl>
2022-12-11 16:37:40 -0300

    ....a

--- js/userAgent.js
+++ js/userAgent.js
@@ -1,7 +1,9 @@
 
 window.addEventListener('load', () => {
     const tipo = document.getElementById(`tipoUserAgent`);
-    if (navigator.userAgent.includes('Mobile')) { // (xxx && 'Windows') se muestra... verificar palabra cable para telefonos
+    if (navigator.userAgent.includes('Windows')) { // (xxx && 'Windows') se muestra... verificar palabra cable para telefonos
+        tipo.innerHTML = `<img src="https://bitacoradelasavesdechile.files.wordpress.com/2011/10/perrito2.jpg" alt="">`
+    } else {
         tipo.innerHTML =    `<a href="http://www.google.com">
                                 <div class="contenedorAPK">
                                    <button class="buttonAnimated">
@@ -9,8 +11,6 @@ window.addEventListener('load', () => {
                                    </button>
                                 </div>
                             </a>`
-    } else {
-        tipo.innerHTML = `<img src="https://bitacoradelasavesdechile.files.wordpress.com/2011/10/perrito2.jpg" alt="">`
     }
 });
 
