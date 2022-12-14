3be48e8702aed7ce1066366563281c049eaa6fb0
Omar Guerra Alfaro <om.guerra@duocuc.cl>
2022-12-14 18:27:23 -0300

    aa

--- js/userAgent.js
+++ js/userAgent.js
@@ -1,6 +1,7 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js";
 import { getStorage, ref, getDownloadURL } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-storage.js";
+
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
